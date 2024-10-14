3bf93cba26e3e9acfa8e697915f7c4fc88a74127
Siddhartha Upadhyay <siddhartupadhya@gmail.com>
2024-10-15 00:50:14 +0530

    CodeGen feature test archives
    Added all remaining test cases for Sauce labs under CodeGen feature to perform ui operation manually and test purely based on Generated Code from CodeGen. no artificial add-ons.

--- PlaywrightWithJava/src/main/java/codeGenFeatureTesting/problemUser_OrderFlow.java
+++ PlaywrightWithJava/src/main/java/codeGenFeatureTesting/problemUser_OrderFlow.java
@@ -1,27 +1,24 @@
 package codeGenFeatureTesting;
 
+import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.*;
 import java.util.regex.Pattern;
 
-import com.microsoft.playwright.Browser;
-import com.microsoft.playwright.BrowserContext;
-import com.microsoft.playwright.BrowserType;
-import com.microsoft.playwright.Locator;
-import com.microsoft.playwright.Page;
-import com.microsoft.playwright.Playwright;
-import com.microsoft.playwright.options.AriaRole;
-
-public class standardUser_OrderFlow {
-
-	public static void main(String args[]) {
+public class problemUser_OrderFlow {
+	public static void main(String[] args) {
 		try (Playwright playwright = Playwright.create()) {
 			Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
 			BrowserContext context = browser.newContext();
 			Page page = context.newPage();
+			page.navigate("https://demo.playwright.dev/todomvc/");
+			page.navigate("https://demo.playwright.dev/todomvc/#/");
 			page.navigate(
-					"https://www.google.com/search?q=suacedemo&oq=suacedemo&gs_lcrp=EgZjaHJvbWUyBggAEEUYOdIBCDIwOTdqMGo0qAIAsAIA&sourceid=chrome&ie=UTF-8");
+					"https://www.google.com/search?q=saucelabs&oq=saucelabs&gs_lcrp=EgZjaHJvbWUyBggAEEUYOdIBCDM5MDVqMGo0qAIAsAIA&sourceid=chrome&ie=UTF-8");
+			page.navigate(
+					"https://www.google.com/search?q=swag+labs&oq=swag+labs&gs_lcrp=EgZjaHJvbWUyBggAEEUYOdIBCDE2MjZqMGo5qAIAsAIB&sourceid=chrome&ie=UTF-8");
 			page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Swag Labs Swag Labs https://")).click();
 			page.locator("[data-test=\"username\"]").click();
-			page.locator("[data-test=\"username\"]").fill("standard_user");
+			page.locator("[data-test=\"username\"]").fill("problem_user");
 			page.locator("[data-test=\"password\"]").click();
 			page.locator("[data-test=\"password\"]").fill("secret_sauce");
 			page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("LOGIN")).click();
@@ -31,14 +28,16 @@ public class standardUser_OrderFlow {
 			page.locator("div")
 					.filter(new Locator.FilterOptions().setHasText(Pattern.compile("^\\$9\\.99ADD TO CART$")))
 					.getByRole(AriaRole.BUTTON).click();
-			page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("2")).click();
 			page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("CHECKOUT")).click();
 			page.locator("[data-test=\"firstName\"]").click();
 			page.locator("[data-test=\"firstName\"]").fill("S");
-			page.locator("[data-test=\"lastName\"]").click();
+			page.locator("[data-test=\"firstName\"]").press("Tab");
 			page.locator("[data-test=\"lastName\"]").fill("U");
 			page.locator("[data-test=\"lastName\"]").press("Tab");
-			page.locator("[data-test=\"postalCode\"]").fill("567676");
+			page.locator("[data-test=\"firstName\"]").click();
+			page.locator("[data-test=\"firstName\"]").fill("S");
+			page.locator("[data-test=\"postalCode\"]").click();
+			page.locator("[data-test=\"postalCode\"]").fill("508988");
 			page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("CONTINUE")).click();
 			page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("FINISH")).click();
 			page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Open Menu")).click();
