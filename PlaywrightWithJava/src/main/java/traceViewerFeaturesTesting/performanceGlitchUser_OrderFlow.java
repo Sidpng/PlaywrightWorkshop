af7f737b8154c20d93ca7d180f7245b249b5602b
Siddhartha Upadhyay <siddhartupadhya@gmail.com>
2024-10-20 00:03:22 +0530

    Traceviewer Feature Completed
    added all other remaining test cases for Swag Labs: Performance_glitch user, Porblem user, there was an issue with the Add to cart button click , fixed it for all the applicable test files.

--- PlaywrightWithJava/src/main/java/traceViewerFeaturesTesting/performanceGlitchUser_OrderFlow.java
+++ PlaywrightWithJava/src/main/java/traceViewerFeaturesTesting/performanceGlitchUser_OrderFlow.java
@@ -1,33 +1,27 @@
 package traceViewerFeaturesTesting;
 
+import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.*;
+
 import java.nio.file.Paths;
 import java.util.regex.Pattern;
 
-import com.microsoft.playwright.Browser;
-import com.microsoft.playwright.BrowserContext;
-import com.microsoft.playwright.BrowserType;
-import com.microsoft.playwright.Locator;
-import com.microsoft.playwright.Page;
-import com.microsoft.playwright.Playwright;
-import com.microsoft.playwright.Tracing;
-import com.microsoft.playwright.options.AriaRole;
-
-public class standardUser_OrderFlow {
-
-	public static void main(String args[]) {
+public class performanceGlitchUser_OrderFlow {
+	public static void main(String[] args) {
 		try (Playwright playwright = Playwright.create()) {
-
 			Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
 			BrowserContext context = browser.newContext();
 
-			context.tracing().start(new Tracing.StartOptions().setScreenshots(true).setSnapshots(true));
+			context.tracing().start(new Tracing.StartOptions().setScreenshots(true).setSnapshots(true)); // tracing
 
 			Page page = context.newPage();
+			page.navigate("https://demo.playwright.dev/todomvc/");
+			page.navigate("https://demo.playwright.dev/todomvc/#/");
 			page.navigate(
-					"https://www.google.com/search?q=suacedemo&oq=suacedemo&gs_lcrp=EgZjaHJvbWUyBggAEEUYOdIBCDIwOTdqMGo0qAIAsAIA&sourceid=chrome&ie=UTF-8");
+					"https://www.google.com/search?q=swag+labs&oq=swag+labs&gs_lcrp=EgZjaHJvbWUyBggAEEUYOdIBCDYwNjVqMGo0qAIAsAIA&sourceid=chrome&ie=UTF-8");
 			page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Swag Labs Swag Labs https://")).click();
 			page.locator("[data-test=\"username\"]").click();
-			page.locator("[data-test=\"username\"]").fill("standard_user");
+			page.locator("[data-test=\"username\"]").fill("performance_glitch_user");
 			page.locator("[data-test=\"password\"]").click();
 			page.locator("[data-test=\"password\"]").fill("secret_sauce");
 			page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("LOGIN")).click();
@@ -43,17 +37,19 @@ public class standardUser_OrderFlow {
 			page.locator("[data-test=\"firstName\"]").fill("S");
 			page.locator("[data-test=\"lastName\"]").click();
 			page.locator("[data-test=\"lastName\"]").fill("U");
-			page.locator("[data-test=\"lastName\"]").press("Tab");
-			page.locator("[data-test=\"postalCode\"]").fill("567676");
+			page.locator("[data-test=\"postalCode\"]").click();
+			page.locator("[data-test=\"postalCode\"]").fill("65676");
 			page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("CONTINUE")).click();
 			page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("FINISH")).click();
 			page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Open Menu")).click();
 			page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Logout")).click();
 
-			context.tracing().stop(new Tracing.StopOptions().setPath(Paths.get("./src/main/java/traceViewerResults/trace.zip")));
+			context.tracing().stop(
+					new Tracing.StopOptions().setPath(Paths.get("./src/main/java/traceViewerResults/tracePGUOF.zip")));
+			//configure trace file location and close tracing as well, before closing context, browser and page.
 
-			context.close();
 			page.close();
+			context.close();
 			browser.close();
 		}
 	}
