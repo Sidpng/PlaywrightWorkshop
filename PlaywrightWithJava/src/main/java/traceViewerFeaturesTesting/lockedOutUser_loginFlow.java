3da5ccd8ff5c5621972d6062900f7f2878e97497
Siddhartha Upadhyay <siddhartupadhya@gmail.com>
2024-10-19 23:49:20 +0530

    Traceviewer Feature Testing update
    Added lockedOut user test case as a part of Traceviewer Feature Testing update

---
+++ PlaywrightWithJava/src/main/java/traceViewerFeaturesTesting/lockedOutUser_loginFlow.java
@@ -0,0 +1,41 @@
+package traceViewerFeaturesTesting;
+
+import java.nio.file.Paths;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.Tracing;
+import com.microsoft.playwright.options.AriaRole;
+
+public class lockedOutUser_loginFlow {
+	public static void main(String[] args) {
+		try (Playwright playwright = Playwright.create()) {
+			Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+			BrowserContext context = browser.newContext();
+
+			context.tracing().start(new Tracing.StartOptions().setScreenshots(true).setSnapshots(true)); // tracing
+																											// initialisation
+
+			Page page = context.newPage();
+			page.navigate("https://www.saucedemo.com/v1/");
+			page.locator("[data-test=\"username\"]").click();
+			page.locator("[data-test=\"username\"]").fill("locked_out_user");
+			page.locator("[data-test=\"username\"]").press("Tab");
+			page.locator("[data-test=\"password\"]").fill("secret_sauce");
+			page.locator("[data-test=\"password\"]").press("Enter");
+			page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("LOGIN")).click();
+
+			context.tracing().stop(
+					new Tracing.StopOptions().setPath(Paths.get("./src/main/java/traceViewerResults/traceLUOF.zip")));
+			//configure trace file location and close tracing as well, before closing context, browser and page.
+
+			context.close();
+			page.close();
+			browser.close();
+
+		}
+	}
+}
